1abdcacd0a3bd85ab4b76f462822319a20437775
cleland <clelandgt@gmail.com>
2017-02-07 22:46:01 +0800

    update

--- src/ip_proxy/settings.py
+++ src/ip_proxy/settings.py
@@ -19,7 +19,7 @@ FAIL_RATE_LIMIT = 0.5
 ON_FAIL_RATE_TIMES = 10
 
 
-UPDATE_TIME = 10  # 每两分钟验证一轮数据库中的IP
+UPDATE_TIME = 2 * 60  # 每两分钟验证一轮数据库中的IP
 IPS_MIN_NUM = 100
 
 
@@ -33,49 +33,49 @@ PARSER_LIST = [
             'pattern': ".//*[@id='main']/div/div[1]/table/tr[position()>1]",
             'position':{'ip': './td[1]', 'port': './td[2]', 'type': './td[4]'}
         },
-        # {
-        #     'urls': ['http://www.66ip.cn/areaindex_%s/%s.html' % (m, n) for m in range(1, 35) for n in range(1, 5)],
-        #     'type':'xpath',
-        #     'pattern': ".//*[@id='footer']/div/table/tr[position()>1]",
-        #     'position':{'ip': './td[1]', 'port': './td[2]', 'type': './td[4]'}
-        # },
-        # {
-        #     'urls': ['http://www.kuaidaili.com/proxylist/%s/' % n for n in range(1, 10)],
-        #     'type': 'xpath',
-        #     'pattern': ".//*[@id='index_free_list']/table/tbody/tr[position()>0]",
-        #     'position':{'ip': './td[1]', 'port': './td[2]', 'type': './td[3]'}
-        # },
-        # {
-        #     'urls': ['http://www.kuaidaili.com/free/%s/%s/' % (m, n) for m in ['inha', 'outha'] for n in range(1, 10)],
-        #     'type':'xpath',
-        #     'pattern': ".//*[@id='list']/table/tbody/tr[position()>0]",
-        #     'position':{'ip': './td[1]', 'port': './td[2]', 'type': './td[3]'}
-        # },
-        # {
-        #     'urls': ['http://www.ip181.com/daili/%s.html' % n for n in range(1, 10)],
-        #     'type':'xpath',
-        #     'pattern': "/html/body/div[2]/div/div[2]/div/div[3]/table/tbody/tr[position()>1]",
-        #     'position': {'ip': './td[1]', 'port': './td[2]', 'type': './td[3]'}
-        #
-        # },
-        # {
-        #     'urls': ['http://www.xicidaili.com/nn/%s' % n for n in range(1, 5)],
-        #     'type':'xpath',
-        #     'pattern': ".//*[@id='ip_list']/tr[position()>1]",
-        #     'position': {'ip': './td[2]', 'port': './td[3]', 'type': './td[5]'}
-        # },
-        # {
-        #     'urls': ['http://ip84.com/%s/%s' % (m, n) for m in ('gn', 'pn') for n in range(1, 10)],
-        #     'type':'xpath',
-        #     'pattern': ".//*[@id='content']/div[1]/table/tr[position()>1]",
-        #     'position': {'ip': './td[1]', 'port': './td[2]', 'type': './td[4]'}
-        # },
-        # {
-        #     'urls': ['http://www.nianshao.me/?stype=2&page=%s' % n for n in range(1, 50)],
-        #     'type': 'xpath',
-        #     'pattern': ".//*[@class='table']/tbody/tr[position()>1]",
-        #     'position': {'ip': './td[1]', 'port': './td[2]', 'type': './td[4]'}
-        # },
+        {
+            'urls': ['http://www.66ip.cn/areaindex_%s/%s.html' % (m, n) for m in range(1, 35) for n in range(1, 5)],
+            'type':'xpath',
+            'pattern': ".//*[@id='footer']/div/table/tr[position()>1]",
+            'position':{'ip': './td[1]', 'port': './td[2]', 'type': './td[4]'}
+        },
+        {
+            'urls': ['http://www.kuaidaili.com/proxylist/%s/' % n for n in range(1, 10)],
+            'type': 'xpath',
+            'pattern': ".//*[@id='index_free_list']/table/tbody/tr[position()>0]",
+            'position':{'ip': './td[1]', 'port': './td[2]', 'type': './td[3]'}
+        },
+        {
+            'urls': ['http://www.kuaidaili.com/free/%s/%s/' % (m, n) for m in ['inha', 'outha'] for n in range(1, 10)],
+            'type':'xpath',
+            'pattern': ".//*[@id='list']/table/tbody/tr[position()>0]",
+            'position':{'ip': './td[1]', 'port': './td[2]', 'type': './td[3]'}
+        },
+        {
+            'urls': ['http://www.ip181.com/daili/%s.html' % n for n in range(1, 10)],
+            'type':'xpath',
+            'pattern': "/html/body/div[2]/div/div[2]/div/div[3]/table/tbody/tr[position()>1]",
+            'position': {'ip': './td[1]', 'port': './td[2]', 'type': './td[3]'}
+
+        },
+        {
+            'urls': ['http://www.xicidaili.com/nn/%s' % n for n in range(1, 5)],
+            'type':'xpath',
+            'pattern': ".//*[@id='ip_list']/tr[position()>1]",
+            'position': {'ip': './td[2]', 'port': './td[3]', 'type': './td[5]'}
+        },
+        {
+            'urls': ['http://ip84.com/%s/%s' % (m, n) for m in ('gn', 'pn') for n in range(1, 10)],
+            'type':'xpath',
+            'pattern': ".//*[@id='content']/div[1]/table/tr[position()>1]",
+            'position': {'ip': './td[1]', 'port': './td[2]', 'type': './td[4]'}
+        },
+        {
+            'urls': ['http://www.nianshao.me/?stype=2&page=%s' % n for n in range(1, 50)],
+            'type': 'xpath',
+            'pattern': ".//*[@class='table']/tbody/tr[position()>1]",
+            'position': {'ip': './td[1]', 'port': './td[2]', 'type': './td[4]'}
+        },
         ]
 
 
