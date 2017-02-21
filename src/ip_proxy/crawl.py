e29d6d77cfcb715acf853e7920f20f062a636edc
cleland <clelandgt@163.com>
2017-02-21 16:57:34 +0800

    remove type attribute in PARSER_LIST and update crawl module

--- src/ip_proxy/crawl.py
+++ src/ip_proxy/crawl.py
@@ -17,19 +17,22 @@ class Crawl(object):
         self.logger = logging.getLogger(__name__)
 
     def run(self, url, parser):
-        resp = self.download(url)
-        return self.parse(resp, parser)
+        try:
+            resp = self.download(url)
+            return self.parse(resp, parser)
+        except Exception as e:
+            self.logger.exception(e)
 
     def download(self, url):
-        for index in xrange(MAX_RETRY_TIMES):
+        for index in xrange(1, MAX_RETRY_TIMES+1):
             try:
-                if index == 0:
+                if index == 1:
                     resp = self.request.get(url=url, timeout=CRAWL_TIMEOUT)
                 else:
                     proxy = self.get_proxy()
                     resp = self.request.get(url=url, timeout=CRAWL_TIMEOUT, proxies=proxy)
                 if not resp.ok:
-                    raise ValueError('response status code is {}, not 200', resp.status_code)
+                    raise ValueError('response status code is {}, not 200'.format(resp.status_code))
                 self.logger.info('connect url {} success.'.format(url))
                 return resp.text
             except Exception as e:
@@ -39,8 +42,6 @@ class Crawl(object):
 
     @staticmethod
     def parse(document, parser):
-        if parser['type'] != 'xpath':
-            raise ValueError('type of parser is {0}, not xpath'.format(parser['type']))
         proxies = []
         root = etree.HTML(document)
         pattern = root.xpath(parser['pattern'])
@@ -49,18 +50,13 @@ class Crawl(object):
             ip = position.xpath(parser['position']['ip'])[0].text
             port = position.xpath(parser['position']['port'])[0].text
             ip_type = position.xpath(parser['position']['type'])[0].text
-            if ip_type.find(u'高匿') != -1:
-                ip_type = '高匿'
-            else:
-                ip_type = '匿名'
-            proxy = {'ip': ip, 'port': int(port), 'ip_type': ip_type, 'protocol': '', 'speeds': []}
-            proxies.append(proxy)
+            ip_type = '高匿' if ip_type.find(u'高匿') != -1 else '匿名'
+            proxies.append({'ip': ip, 'port': int(port), 'ip_type': ip_type, 'protocol': '', 'speeds': []})
         return proxies
 
     def get_proxy(self):
         if len(self.proxies) == 0:
             proxies = IpProxies.objects.all()
             proxies = ranking(proxies)
-            for proxy in proxies:
-                self.proxies.append({'http': 'http://{}'.format(proxy[0])})
+            self.proxies = [{'http': 'http://{}'.format(proxy[0])} for proxy in proxies]
         return self.proxies.pop()
